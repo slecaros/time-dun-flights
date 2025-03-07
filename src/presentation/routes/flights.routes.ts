b644db13caac9902a221f96bd2bab55255be30fa
Sergio Lecaros <slecaros@uc.cl>
2025-03-06 22:52:59 -0300

    ✨ feat: get flight

--- src/presentation/routes/flights.routes.ts
+++ src/presentation/routes/flights.routes.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express';
-import { FlightsController } from '../api/controllers/flights.controller';
 import { handler as createFlightHandler } from '../api/flights/create-flight';
 import { handler as getAllFlightsHandler } from '../api/flights/get-all-flights';
+import { handler as getFlightHandler } from '../api/flights/get-flight';
+
 const router = Router();
 
 // GET /api/flights
@@ -10,8 +11,9 @@ router.get('/', (req, res) => getAllFlightsHandler(req, res));
 // POST /api/flights
 router.post('/', (req, res) => createFlightHandler(req, res));
 
-// You can add more routes here:
-// router.get('/:id', (req, res) => flightsController.getById(req, res));
+// GET /api/flights/:flightCode
+router.get('/:flightCode', (req, res) => getFlightHandler(req, res));
+
 // router.put('/:id', (req, res) => flightsController.update(req, res));
 // router.delete('/:id', (req, res) => flightsController.delete(req, res));
 
